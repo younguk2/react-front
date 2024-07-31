5c9dda55dbc4da2447b85260aa570897c8266264
younguk2 <neostory1515@gmail.com>
2024-07-31 15:48:48 +0900

    리액트 프로젝트 빌드

---
+++ src/components/Test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Test = () => {
+	return (
+		<div className='test'>
+			<div className='test__inner'>
+				<div className='test__title'>
+					<h3>Test Title</h3>
+					<div className='test__lines'>
+						<span className='line'></span>
+						<span className='line'></span>
+						<span className='line'></span>
+						<span className='line'></span>
+						<span className='line'></span>
+						<span className='line'></span>
+						<span className='line'></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
